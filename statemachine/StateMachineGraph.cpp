e31dee754c1a94e7f1063145f55e86eb9521a856
Andrey <algebroid.2011@gmail.com>
2015-12-07 01:36:09 +0300

    Почти готов Ахо-Корасик, где то небольшая ошибка.

--- statemachine/StateMachineGraph.cpp
+++ statemachine/StateMachineGraph.cpp
@@ -3,3 +3,94 @@
 //
 
 #include "StateMachineGraph.h"
+// Конструктор графа
+StateMachineGraph :: StateMachineGraph() {
+    // Инициализация бора
+    nodes.push_back(stateMachineVertex(-1, '\0'));
+}
+// Возвращает умную суффиксную ссылку данной вершины
+int StateMachineGraph :: getSmartSuffixLink(int v) {
+    if (nodes[v].smartSuffixLink == -1) {
+        int u = getSuffixLink(v);
+        // Если обычная суффиксная ссылка от v указывает на корень
+        if (u == 0) {
+            nodes[v].smartSuffixLink = 0;
+        } else {
+            // А если указывает не на корень, то
+            if (nodes[u].isWord) {
+                // если u соответствует слову в боре, то умная ссылка найдена
+                nodes[v].smartSuffixLink = u;
+            } else {
+                // иначе, ищем умную ссылку от u
+                nodes[v].smartSuffixLink = getSmartSuffixLink(u);
+            }
+        }
+    }
+    return nodes[v].smartSuffixLink;
+}
+// Возвращает суффиксную ссылку заданной вершины
+int StateMachineGraph :: getSuffixLink(int v) {
+    // Если суффиксной ссылки у этой вершины ещё нет, получим её
+    if (nodes[v].suffixLink == -1) {
+        // Если вершина - корень, или её предок - корень, то
+        // её суффиксная ссылка - это корень.
+        if (nodes[v].parent == 0 || v == 0)
+            nodes[v].suffixLink = 0;
+        else {
+            nodes[v].suffixLink = getMove(getSuffixLink(nodes[v].parent), nodes[v].parentSymbol);
+        }
+    }
+    return nodes[v].suffixLink;
+}
+// Возвращает самый оптимальный переход, с учётом суффиксных ссылок
+// из заданной вершины по заданному символу
+int StateMachineGraph :: getMove(int v, char ch) {
+    // Это если оптимальный переход пока не задан. Зададим его.
+    if (nodes[v].move.find(ch) == nodes[v].move.end()) {
+        // Если есть ребёнок по символу ch
+        if (nodes[v].child.find(ch) != nodes[v].child.end())
+            // То этот переход будет по этому ребёнку
+            nodes[v].move[ch] = nodes[v].child[ch];
+            // А если ребёнка по символу ch нет
+        else
+            // Если это корень, то?? как так может быть
+        if (v == 0)
+            nodes[v].move[ch] = 0;
+            // Иначе переходим по суффиксной ссылке, и уже ищем там.
+        else
+            nodes[v].move[ch] = getMove(getSuffixLink(v), ch);
+    }
+    return nodes[v].move[ch];
+}
+// Вставляет заданную строку в бор
+void StateMachineGraph :: insertPattern(const string &name) {
+    // Вставка строки в бор
+    int active = 0; // Все дела начинаются с корня
+    for (int i = 0; i < name.length(); ++i) {
+        char symbol = name[i];
+        if (nodes[active].child.find(symbol) == nodes[active].child.end()) {
+            // Значит, такого ребра не существует
+            // Добавим его!
+            nodes.push_back(stateMachineVertex(active, symbol));
+            nodes[active].child[symbol] = (int)nodes.size() - 1;
+        }
+        active = nodes[active].child[symbol];
+    }
+    nodes[active].isWord = true;
+    nodes[active].pattern = name;
+}
+// Возвращает ответ в виде мультимапа - <строка, начала её вхождений>
+multimap <string, int> StateMachineGraph :: solve(string input) {
+    int u = 0;
+    multimap <string, int> answer;
+    for (int i = 0; i < input.length(); ++i) {
+        u = getMove(u, input[i]);
+        for (int v = u; v != 0; v = getSuffixLink(v)) {
+            if (nodes[v].isWord) {
+                answer.insert(make_pair(nodes[v].pattern, i));
+                cout << nodes[v].pattern << " " << i << endl;
+            }
+        }
+    }
+    return answer;
+}
--- statemachine/StateMachineGraph.h
+++ statemachine/StateMachineGraph.h
@@ -5,9 +5,11 @@
 #ifndef STATEMACHINE_STATEMACHINEGRAPH_H
 #define STATEMACHINE_STATEMACHINEGRAPH_H
 
+#include <iostream>
 #include <map>
 #include <vector>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -33,90 +35,29 @@ private:
     vector<stateMachineVertex> nodes;
     void doNothing() {};
 public:
-    StateMachineGraph() {
-        // Инициализация бора
-        nodes.push_back(stateMachineVertex(-1, '\0'));
-    }
-
+    // Конструктор графа
+    StateMachineGraph();
     // Необходимые функции для паттерна графа
     void insert_vertex(const string& name) { insertPattern(name); }
     void add_edge(const string& from, const string& to, double weight = 0) { doNothing(); }
     void transpose() { doNothing();};
     vector<vector<string>> getStrongComponents() {
+        doNothing();
         return vector<vector<string>>();
     }
 
+    // Возвращает ответ в виде мультимапа - <строка, начала её вхождений>
+    multimap <string, int> solve(string input);
 private:
-    int getSmartSuffixLink(int v) {
-        if (nodes[v].smartSuffixLink == -1) {
-            int u = getSuffixLink(v);
-            // Если обычная суффиксная ссылка от v указывает на корень
-            if (u == 0) {
-                nodes[v].smartSuffixLink = 0;
-            } else {
-                // А если указывает не на корень, то
-                if (nodes[u].isWord) {
-                    // если u соответствует слову в боре, то умная ссылка найдена
-                    nodes[v].smartSuffixLink = u;
-                } else {
-                    // иначе, ищем умную ссылку от u
-                    nodes[v].smartSuffixLink = getSmartSuffixLink(u);
-                }
-            }
-        }
-        return nodes[v].smartSuffixLink;
-    }
+    // Возвращает умную суффиксную ссылку данной вершины
+    int getSmartSuffixLink(int v);
     // Возвращает суффиксную ссылку заданной вершины
-    int getSuffixLink(int v) {
-        // Если суффиксной ссылки у этой вершины ещё нет, получим её
-        if (nodes[v].suffixLink == -1) {
-            // Если вершина - корень, или её предок - корень, то
-            // её суффиксная ссылка - это корень.
-            if (nodes[v].parent == 0 || v == 0)
-                nodes[v].suffixLink = 0;
-            else {
-                nodes[v].suffixLink = getMove(getSuffixLink(nodes[v].parent), nodes[v].parentSymbol);
-            }
-        }
-        return nodes[v].suffixLink;
-    }
+    int getSuffixLink(int v);
     // Возвращает самый оптимальный переход, с учётом суффиксных ссылок
     // из заданной вершины по заданному символу
-    int getMove(int v, char ch) {
-        // Это если оптимальный переход пока не задан. Зададим его.
-        if (nodes[v].move.find(ch) == nodes[v].move.end()) {
-            // Если есть ребёнок по символу ch
-            if (nodes[v].child.find(ch) != nodes[v].child.end())
-                // То этот переход будет по этому ребёнку
-                nodes[v].move[ch] = nodes[v].child[ch];
-            // А если ребёнка по символу ch нет
-            else
-                // Если это корень, то?? как так может быть
-                if (v == 0)
-                    nodes[v].move[ch] = 0;
-                // Иначе переходим по суффиксной ссылке, и уже ищем там.
-                else
-                    nodes[v].move[ch] = getMove(getSuffixLink(v), ch);
-        }
-        return nodes[v].move[ch];
-    }
+    int getMove(int v, char ch);
     // Вставляет заданную строку в бор
-    void insertPattern(const string &name) {
-        // Вставка строки в бор
-        int active = 0; // Все дела начинаются с корня
-        for (int i = 0; i < name.length(); ++i) {
-            char symbol = name[i];
-            if (nodes[active].child.find(symbol) == nodes[active].child.end()) {
-                // Значит, такого ребра не существует
-                // Добавим его!
-                nodes.push_back(stateMachineVertex(active, symbol));
-                nodes[active].child[symbol] = (int)nodes.size() - 1;
-            }
-            active = nodes[active].child[symbol];
-        }
-        nodes[active].isWord = true;
-        nodes[active].pattern = name;
-    }
+    void insertPattern(const string &name);
 };
 
 
