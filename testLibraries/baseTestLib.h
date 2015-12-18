2693cd26275eaace823351aa6466bbfe6ac0d8ba
Andrey <algebroid.2011@gmail.com>
2015-12-18 13:42:16 +0300

    base test library added

---
+++ testLibraries/baseTestLib.h
@@ -0,0 +1,12 @@
+#ifndef BASETESTLIB_H
+#define BASETESTLIB_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using std::string;
+
+//Working with tests
+void findTest(string taskname, string name, std :: ifstream& ifs);
+#endif // BASETESTLIB_H
