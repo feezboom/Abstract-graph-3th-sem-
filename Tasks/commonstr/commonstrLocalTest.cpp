9eb5d578905fcbd22f69f72034a5e60724f36dca
Andrey <algebroid.2011@gmail.com>
2016-01-18 16:27:21 +0300

    bla

--- Tasks/commonstr/commonstrLocalTest.cpp
+++ Tasks/commonstr/commonstrLocalTest.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-    LongestCommonSubstring suffixArray(string("asdf"), string("asdf"), string("asdff"));
-//    cout << suffixArray.toString() << endl;
-
+    LongestCommonSubstring suffixArray(string("abcd"), string("bc"), string("cd"));
+    cout << suffixArray.toString() << endl;
+    suffixArray.printArrays();
+/*
     string s1 = "banana", s2 = "ananas", s3 = "marihuana";
     suffixArray.update(s1, s2, s3);
     suffixArray.printArrays();
     cout << suffixArray.toString() << endl;
-    cout << endl;
+    cout << endl;*/
     return 0;
 }
